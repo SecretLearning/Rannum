65c1fffd85154a4c6ef9b4016c6533366fd9db0b
SecretLearning <145436806+SecretLearning@users.noreply.github.com>
2024-02-17 17:31:45 +0100

    Ori script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function generatePassword() {
     let password = parseInt(digitArray.join(''));
 
     // Display randomNumber to user
-    display.innerHTML = 2
+    display.innerHTML = password
 }
 
 // Activates function once button is clicked
